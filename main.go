6cb6940c0b4459273346b2a0dc7db5bd1663b176
Adphi <philippe.adrien.nousse@gmail.com>
2021-10-11 11:52:44 +0200

    add optional feature flag

--- main.go
+++ main.go
@@ -17,13 +17,16 @@ package main
 import (
 	pgs "github.com/lyft/protoc-gen-star"
 	pgsgo "github.com/lyft/protoc-gen-star/lang/go"
+	"google.golang.org/protobuf/types/pluginpb"
 
 	"go.linka.cloud/protoc-gen-go-fields/fields"
 )
 
 func main() {
+	feat := uint64(pluginpb.CodeGeneratorResponse_FEATURE_PROTO3_OPTIONAL)
 	pgs.Init(
 		pgs.DebugEnv("DEBUG"),
+		pgs.SupportedFeatures(&feat),
 	).RegisterModule(
 		fields.Module(),
 	).RegisterPostProcessor(
